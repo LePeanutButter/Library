89566477d86fc48e41de870d4651d6f072aa7bb3
Santiago Botero <157855016+LePeanutButter@users.noreply.github.com>
2025-02-05 08:35:35 -0500

    Library

---
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -0,0 +1,19 @@
+package edu.eci.cvds.tdd.library;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class LibraryTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
